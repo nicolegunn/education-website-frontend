2e4c138e44d363144bb81a77c2fb1e63c43d330e
Eunhye <helloeunhyek@gmail.com>
2024-03-19 17:12:47 +1300

    initial set up for folders and files, router, universal css

---
+++ src/pages/HelpRequests/HelpRequests.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HelpRequests() {
+  return (
+    <div>HelpRequests</div>
+  )
+}
---
+++ src/pages/MakeProject/MakeProject.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MakeProject() {
+  return (
+    <div>MakeProject</div>
+  )
+}
