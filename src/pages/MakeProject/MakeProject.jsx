bbb0b13ba619e57534b7ab398421946311514a14
Anjali Elias <anjalieliasnz@gmail.com>
2024-04-15 12:58:02 +1200

    imported sidebar and footer

--- src/pages/MakeProject/MakeProject.jsx
+++ src/pages/MakeProject/MakeProject.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import makeprojectimage from "../../assets/StudentDashboard/makeProject-screenshot.png"
-import styles from "./MakeProject.module.css"
-import DashboardNavbar from '../../common/DashboardNavbar'
+import React from "react";
+import makeprojectimage from "../../assets/StudentDashboard/makeProject-screenshot.png";
+import styles from "./MakeProject.module.css";
+import DashboardNavbar from "../../common/DashboardNavbar";
+import SideBar from "../../common/SideBar";
+import DashboardFooter from "../../common/DashboardFooter";
 export default function MakeProject() {
   return (
     <>
-    {/*Displays StudentDashboard Navigation bar */}
-     <DashboardNavbar projectTitle='Project' subTitle='Introduction' dotMap="⚫⚫⚫⚫⚫⚫⚫⚫⚫⚫⚫"/>
-    {/*Displays Sidebar and buttons */}
-        <div className={styles.Layout}>
-            <div className={styles.Colunmn}>
-                <div className={styles.Sidebar}>
-                    
-                </div>
-            </div>
-
-    {/*Displays main content on student Dashboard */}
-    <div className={styles.Colunmn}>
-                <div className={styles.CentreArea}>
-                <div className={styles.Dashboard}>
-            <div> {/*Displays Right side of Side bar */}
-    <div className={styles.ImageContainer}>
-    <img className={styles.Image} src={makeprojectimage}alt=""/>
-    </div>
-    </div> 
-                </div>
-            </div>
-            </div>
+      {/*Displays StudentDashboard Navigation bar */}
+      <DashboardNavbar projectTitle="Project" subTitle="Introduction" />
+      {/*Displays Sidebar and buttons */}
+      <div className={styles.Layout}>
+        <div className={styles.Colunmn}>
+          {/* <div className={styles.Sidebar}></div> */}
+          <SideBar dashboard="student" />
         </div>
-        
+
+        {/*Displays main content on student Dashboard */}
+        <div className={styles.Colunmn}>
+          <div className={styles.CentreArea}>
+            <div className={styles.Dashboard}>
+              <div>
+                {" "}
+                {/*Displays Right side of Side bar */}
+                <div className={styles.ImageContainer}>
+                  <img className={styles.Image} src={makeprojectimage} alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <DashboardFooter />
+      </div>
     </>
-    
-  )
+  );
 }
