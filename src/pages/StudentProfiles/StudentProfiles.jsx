23c2365c0abf6cc98c199a8c2eeeb5ae32693dbc
maata <maatat@missionreadyhq.com>
2024-04-15 14:00:47 +1200

    commiting changes

--- src/pages/StudentProfiles/StudentProfiles.jsx
+++ src/pages/StudentProfiles/StudentProfiles.jsx
@@ -1,49 +1,38 @@
-import React from 'react';
-import styles from './StudentProfile.module.css';
-import Footer from '/src/common/DashboardFooter.jsx';
-import DashboardNavbar from '../../common/DashboardNavbar.jsx'; 
-import SideBar from '../../common/SideBar';
-
-
+import {useState, useEffect} from "react";
+import styles from "./StudentProfile.module.css";
+import SideBar from "../../common/SideBar";
 
 
 function StudentProfiles() {
-  const students = [
-    'Aiden Andrews',
-    'Alice Kindellan',
-    'Courtney Bristol',
-    'Hanu Nepe',
-    'Harry McGrath',
-    'Javier Fuego',
-    'Lisa Horan',
-    'Lucia Mendez',
-    'Mark OLeary',
-    'Nagani Cortes',
-    'Neveah Machenry',
-    'Rawiri Fletcher',
-    'Shane OMonahan',
-    'Simon Laine',
-    'Tokio Han'
-
-  ] 
-
-  return (
-    <div>
-      <DashboardNavbar />
+    const [students, setStudents] = useState([]);
+  
+    useEffect(() => {
+      fetch('http://localhost:4000/student')
+        .then((response) => response.json())
+        .then((data) => {
+          setStudents(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }, []);
+  
+    return (
       <div className={styles.App}>
-        <SideBar dashboard="student" />
+        <SideBar dashboard="teacher"/>
         <div className={styles.Rectangle}>
           {students.map((student, index) => (
-            <div key={index} className={styles.StudentCard}>
-            <img className={styles.StudentImg} src={'/images/students/' + student.replace(/\s+/g, '') + '.png'} alt=" "/>
-            <p className={styles.StudentName}>{student}</p>
+            <div className={styles.StudentCard}>
+            <h2 className={styles.StudentName}>{student.name}</h2>
+            <img className={styles.StudentImg} key={index} src={student.profile_pic} alt={`Student ${index + 1}`} />
             </div>
-            ))}
+          ))}
+          
         </div>
-        <Footer />
       </div>
-    </div>
-  )
-}
+    );
+  };
+  
+  
 
 export default StudentProfiles;
