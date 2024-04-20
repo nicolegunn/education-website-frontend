ac39726366b587bddf1526281caab1d996471d49
Eunhye <helloeunhyek@gmail.com>
2024-04-20 21:47:57 +1200

    deleted old side bar files and add minor changes to the side bar

--- src/common/SideBarButtons.jsx
+++ src/common/SideBarButtons.jsx
@@ -6,8 +6,10 @@ export default function SideBarButtons(props) {
     return (
         <Link to={props.link} className={styles.SideBarButtonsLink}>
             <div className={styles.SideBarButtonsList}>
-                <img src={props.image} className={styles.SideBarButtonIcon} alt=""/>
-                <p className={styles.SideBarButtonText}>{props.buttonText}</p>
+                <img src={props.image} className={styles.SideBarButtonIcon} alt="" />
+                <div className={styles.SideBarButtonTextContainer}>
+                    <p className={styles.SideBarButtonText}>{props.buttonText}</p>
+                    </div>
             </div>
         </Link>
     )
